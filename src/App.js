00b4d572af3c33e9b9d08554efef743ca292ec7b
Yash Yadav <yashyadav3701@gmail.com>
2023-01-07 23:09:48 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
 import './App.css';
 import LoadingBar from 'react-top-loading-bar'
-
-import React, { Component } from 'react'
+import React,{useState} from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
 } from "react-router-dom";
 
-export default class App extends Component {
-  pageSize=5;
+const App= ()=>{
+  const pageSize=5;
   // apiKey=process.env.API_KEY;
-  state={
-    progress:0
-  }
-  setprogress=(progress)=>{
-    this.setState({progress:progress});
-  }
-  render() {
+  const [progress, setProgress] = useState(0);
     return (
       <div>
         <Router>
-        <NavBar/>
+          <NavBar/>
         <LoadingBar
         height={3}
         color='#f11946'
-        progress={this.state.progress}
+        progress={progress}
       />
-        <Switch>
-          <Route exact path="/"><News setprogress={this.setprogress} key="general" pageSize={this.pageSize} country='in' category='general'/></Route>
-          <Route exact path="/business"><News setprogress={this.setprogress} key="business" pageSize={this.pageSize} country='in' category='business'/></Route>
-          <Route exact path="/entertainment"><News setprogress={this.setprogress} key="entertainment" pageSize={this.pageSize} country='in' category='entertainment'/></Route>
-          <Route exact path="/general"><News setprogress={this.setprogress} key="general" pageSize={this.pageSize} country='in' category='general'/></Route>
-          <Route exact path="/health"><News setprogress={this.setprogress} key="health" pageSize={this.pageSize} country='in' category='health'/></Route>
-          <Route exact path="/science"><News setprogress={this.setprogress} key="science" pageSize={this.pageSize} country='in' category='science'/></Route>
-          <Route exact path="/sports"><News setprogress={this.setprogress} key="sports" pageSize={this.pageSize} country='in' category='sports'/></Route>
-          <Route exact path="/technology"><News setprogress={this.setprogress} key="technology" pageSize={this.pageSize} country='in' category='technology'/></Route>
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<News setProgress={setProgress} key="general" pageSize={pageSize} country='in' category='general'/>}/>
+          <Route exact path="/business" element={<News setProgress={setProgress} key="business" pageSize={pageSize} country='in' category='business'/>}/>
+          <Route exact path="/entertainment" element={<News setProgress={setProgress} key="entertainment" pageSize={pageSize} country='in' category='entertainment'/>}/>
+          <Route exact path="/general" element={<News setProgress={setProgress} key="health" pageSize={pageSize} country='in' category='health'/>}/>
+          <Route exact path="/science" element={<News setProgress={setProgress} key="science" pageSize={pageSize} country='in' category='science'/>}/>
+          <Route exact path="/sports" element={<News setProgress={setProgress} key="sports" pageSize={pageSize} country='in' category='sports'/>}/>
+          <Route exact path="/technology" element={<News setProgress={setProgress} key="technology" pageSize={pageSize} country='in' category='technology'/>}/>
+        </Routes>
         </Router>
       </div>
     )
   }
-}
 
+  export default App;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import loading from './loading.gif'
-function Spinner() {
+const Spinner=()=> {
   return (
     <div className='text-center'>
       <img className="my-3" src={loading} alt="Loading" />
